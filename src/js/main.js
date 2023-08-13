665e391f944a127d2a22fa0ebb04cf2fb0cb2cab
SD91PL <sd91pl@gmail.com>
2023-08-13 12:27:01 +0200

    made js for nav and nav-info

--- src/js/main.js
+++ src/js/main.js
@@ -19,10 +19,34 @@ const prepareDOMElements = () => {
 
 const prepareDOMEvents = () => {
 	def.addEventListener('mouseover', hoverDef)
+	bts.addEventListener('mouseover', hoverBts)
+	repo.addEventListener('mouseover', hoverRepo)
+	mockup.addEventListener('mouseover', hoverMockup)
+
+    def.addEventListener('mouseleave', hoverOut)
+    bts.addEventListener('mouseleave', hoverOut)
+    repo.addEventListener('mouseleave', hoverOut)
+    mockup.addEventListener('mouseleave', hoverOut)
 }
 
 const hoverDef = () => {
-    console.log('test');
+    navInfo.textContent = 'A default version of the site - without Bootstrap'
+}
+
+const hoverBts = () => {
+    navInfo.textContent = 'A version of the website developed using Bootstrap v5.3'
+}
+
+const hoverRepo = () => {
+    navInfo.textContent = 'Full repository for creating both versions of the website'
+}
+
+const hoverMockup = () => {
+    navInfo.textContent = 'A mock-up based on which the websites were made'
+}
+
+const hoverOut = () => {
+    navInfo.textContent = 'Place the cursor for more information'
 }
 
 document.addEventListener('DOMContentLoaded', main)
