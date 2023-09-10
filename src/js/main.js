e2a84b884f94d2de856e693c53bf60205e7f1696
SD91PL <sd91pl@gmail.com>
2023-09-10 18:52:52 +0200

    menuFG updated - after fixing the site after Code Review

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 let navInfo
 let def
+let defBEM
 let bts
 let repo
+let repoACR
 let mockup
 
 const main = () => {
@@ -12,20 +14,26 @@ const main = () => {
 const prepareDOMElements = () => {
 	navInfo = document.querySelector('.nav-info')
 	def = document.querySelector('.default')
+	defBEM = document.querySelector('.default-bem')
 	bts = document.querySelector('.bootstrap')
 	repo = document.querySelector('.repo')
+	repoACR = document.querySelector('.repo-acr')
 	mockup = document.querySelector('.mockup')
 }
 
 const prepareDOMEvents = () => {
 	def.addEventListener('mouseover', hoverDef)
+	defBEM.addEventListener('mouseover', hoverDefBEM)
 	bts.addEventListener('mouseover', hoverBts)
 	repo.addEventListener('mouseover', hoverRepo)
+	repoACR.addEventListener('mouseover', hoverRepoACR)
 	mockup.addEventListener('mouseover', hoverMockup)
 
     def.addEventListener('mouseleave', hoverOut)
+    defBEM.addEventListener('mouseleave', hoverOut)
     bts.addEventListener('mouseleave', hoverOut)
     repo.addEventListener('mouseleave', hoverOut)
+    repoACR.addEventListener('mouseleave', hoverOut)
     mockup.addEventListener('mouseleave', hoverOut)
 }
 
@@ -33,12 +41,20 @@ const hoverDef = () => {
     navInfo.textContent = 'A default version of the site - without Bootstrap'
 }
 
+const hoverDefBEM = () => {
+    navInfo.textContent = 'Latest, fixed version of the default site, now with BEM classes - after Code Review'
+}
+
 const hoverBts = () => {
     navInfo.textContent = 'A version of the website developed using Bootstrap v5.3'
 }
 
 const hoverRepo = () => {
-    navInfo.textContent = 'Full repository for creating both versions of the website'
+    navInfo.textContent = 'Full repository for creating both versions of the website - before Code Review'
+}
+
+const hoverRepoACR = () => {
+    navInfo.textContent = 'Repository of the fixed default site with BEM classes - after Code Review'
 }
 
 const hoverMockup = () => {
